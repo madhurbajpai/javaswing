b2591ad77cd9ed538852463e3a6e036805e352ad
madhurbajpai <mdhrbajpai@gmail.com>
2023-05-13 18:38:09 +0530

    completed

--- Calculator.java
+++ Calculator.java
@@ -11,12 +11,17 @@ public class Calculator extends JFrame implements ActionListener {
     JTextField tf1;
     JLabel l1;
     JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16;
+    String txt = "";
+    Double op1, op2;
+    String operator, s0, s1, s2;
 
     Calculator() {
         // Input Panel
-
+        s0 = "";
+        s1 = "";
+        s2 = "";
         inpPan = new JPanel(new GridLayout(1, 1));
-        tf1 = new JTextField(40);
+        tf1 = new JTextField(30);
         tf1.setEditable(false);
         inpPan.add(tf1);
         add(inpPan);
@@ -106,8 +111,53 @@ public class Calculator extends JFrame implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
         String op = ((JButton) e.getSource()).getText();
+        if ((op.charAt(0) >= '0' && op.charAt(0) <= '9' || op.charAt(0) == '.')) {
+            if (!s1.equals(""))
+                s2 = s2 + op;
+            else
+                s0 = s0 + op;
+            txt = s0 + s1 + s2;
+            tf1.setText("" + txt);
+        } else if (op.charAt(0) == 'C') {
+            s0 = s1 = s2 = "";
+            tf1.setText(s0 + s1 + s2);
+        } else if (op.charAt(0) == '=') {
+            double te = 0d;
+            if (s1.equals("+"))
+                te = (Double.parseDouble(s0) + Double.parseDouble(s2));
+            else if (s1.equals("-"))
+                te = (Double.parseDouble(s0) - Double.parseDouble(s2));
+            else if (s1.equals("*"))
+                te = (Double.parseDouble(s0) * Double.parseDouble(s2));
+            if (s1.equals("/"))
+                te = (Double.parseDouble(s0) / Double.parseDouble(s2));
 
-        System.out.println("clicked" + op);
+            tf1.setText(s0 + s1 + s2 + "=" + te);
+
+            s0 = Double.toString(te);
+            s1 = s2 = "";
+        } else {
+            if (s1.equals("") || s2.equals(""))
+                s1 = op;
+            else {
+                double te = 0d;
+                if (s1.equals("+"))
+                    te = (Double.parseDouble(s0) + Double.parseDouble(s2));
+                else if (s1.equals("-"))
+                    te = (Double.parseDouble(s0) - Double.parseDouble(s2));
+                else if (s1.equals("*"))
+                    te = (Double.parseDouble(s0) * Double.parseDouble(s2));
+                if (s1.equals("/"))
+                    te = (Double.parseDouble(s0) / Double.parseDouble(s2));
+
+                tf1.setText(s0 + s1 + s2 + "=" + te);
+
+                s0 = Double.toString(te);
+                s1 = op;
+                s2 = "";
+            }
+        }
+        tf1.setText(s0 + s1 + s2);
 
     }
 
